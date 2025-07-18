463c93aaef655ca4363ad76931fc0cb3d4bfedc9
Folombas <folombas@yandex.ru>
2025-07-18 23:03:07 +0300

    Изучение интерфесов в Go

--- main/main.go
+++ main/main.go
@@ -2,80 +2,38 @@ package main
 
 import "fmt"
 
-// Объявляем интерфейс Flyer с методом Fly()
-type Flyer interface {
-	Fly() string
+// Инерфейс для всех поваров
+type Chef interface {
+	Cook(dish string) string
+	Speciality() string
 }
 
-// Структура Bird с собственным методом Fly()
-type Bird struct {
-	Name  string
-	Wings int
+// Интерфейс для оборудования
+type Appliace interface {
+	Use() string
 }
 
-func (b Bird) Fly() string {
-	return fmt.Sprintf("%s летит, махая %d крыльями", b.Name, b.Wings)
+// Повар-грильщик
+type GrillChef struct {
+	name 		string
+	grillType 	string
 }
 
-// Структура Airplane с собственным методом Fly()
-type Airplane struct {
-	Model string
+func (gc GrillChef) Cook(dish string) string {
+	return fmt.Sprintf("%s готовит на %s: %s", gc.name, gc.grillType, dish)
 }
 
-func (a Airplane) Fly() string {
-	return fmt.Sprintf("Самолет %s летит на реактивной тяге", a.Model)
+func (g GrillChef) Speciality() string {
+	return "стейки и барбекю"
 }
 
-// Структура Superman (демонстрация неявной реализации)
-type Superman struct {
-	PowerLevel int
+func (g GrillChef) Use() string {
+	return fmt.Sprintf("Разжигае %s", g.grillType)
 }
 
-func (s Superman) Fly() string {
-	return fmt.Sprintf("Супермен летит с силой %d%%", s.PowerLevel)
+// Кондитер 
+type PastChef struct {
+	name 		string
+	ovenTemp 	int
 }
 
-// Функция принимающая ЛЮБОЙ объект, реализующий Flyer
-func StartFlight(f Flyer) {
-	fmt.Println(f.Fly())
-}
-
-func main() {
-	// Создаем объекты разных типов
-	sparrow := Bird{"Воробей", 2}
-	boeing := Airplane{"Boeing 747"}
-	clark := Superman{100}
-
-	// Вызываем метод Fly() напрямую
-	fmt.Println(sparrow.Fly())
-	fmt.Println(boeing.Fly())
-	fmt.Println(clark.Fly())
-
-	fmt.Println("\nЧерез интерфейс:")
-	// Используем интерфейсный тип
-	var flyer Flyer
-
-	flyer = sparrow // Bird реализует Flyer
-	StartFlight(flyer)
-
-	flyer = boeing // Airplane реализует Flyer
-	StartFlight(flyer)
-
-	flyer = clark // Superman реализует Flyer
-	StartFlight(flyer)
-
-	// Демонстрация полиморфизма
-	fmt.Println("\nПолиморфизм в срезе:")
-	flyingObjects := []Flyer{
-		Bird{"Орел", 2},
-		Airplane{"Сухой Суперджет"},
-		Superman{85},
-	}
-
-	for _, obj := range flyingObjects {
-		fmt.Println(obj.Fly())
-	}
-}
-
-// Пример показывает, как совершенно разные сущности (птицы, самолеты, супергерои)
-// могут быть объединены общим поведением (умением летать) через интерфейс.
